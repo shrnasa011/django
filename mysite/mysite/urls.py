f5aae994375a2a838f024b8236f3dcf4687c904a
Shreya Nasa <shrnasa011@gmail.com>
2016-06-27 02:54:54 +0530

    Models created

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
+#from django.conf.urls.defaults import *
 from django.contrib import admin
+from mysite.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^polls/', include('polls.urls')),
+    url(r'^hello/$', hello ),
+    url(r'^now/$', current_datetime ),
+    url(r'^time/plus(\d{1,2})/$', datetimeplus ),
 ]
