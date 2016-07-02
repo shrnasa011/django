cf6541200236d30a29e51c9ffecfa00636d9a5e7
Shreya Nasa <shrnasa011@gmail.com>
2016-07-02 11:30:47 +0530

    Tests created

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,69 @@
+import datetime
 from django.test import TestCase
-
+from django.utils import timezone
+from polls.models import Question, Choice
+from django.core.urlresolvers import reverse
 # Create your tests here.
+
+class QuestionMethodTests(TestCase):
+
+    def test_future_question(self):
+        time=timezone.now()+datetime.timedelta(days=30)
+        future_question=Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertEqual(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertEqual(recent_question.was_published_recently(), True)
+
+def create_question(question_text, days):
+    time=timezone.now()+datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionViewTests(TestCase):
+    def test_index_null(self):
+        response=self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_question(self):
+        create_question(question_text="Past question.", days=-30)
+        response=self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question.>'])
+
+    def test_future_question(self):
+        create_question(question_text="Future question.", days=30)
+        response=self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.", status_code=200)
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_and_future(self):
+        create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response=self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question.>'])
+
+    def test_two_past_questions(self):
+        create_question(question_text="Past question 1.", days=-30)
+        create_question(question_text="Past question 2.", days=-5)
+        response=self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question 2.>', '<Question: Past question 1.>'])
+
+
+class QuestionIndexDetailTests(TestCase):
+    def test_future_ques_detail(self):
+        future=create_question(question_text='Future question', days=5)
+        response=self.client.get(reverse('polls:detail',args=(future.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_ques_detail(self):
+        past=create_question(question_text='Past question', days=-5)
+        response=self.client.get(reverse('polls:detail',args=(past.id,)))
+        self.assertContains(response, past.question_text, status_code=200)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,21 +2,43 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from polls.models import Question, Choice
 from django.core.urlresolvers import reverse
+from django.utils import timezone
+
+from django.views import generic
 
 # Create your views here.
-def index(request):
-    latest_ques=Question.objects.order_by('-pub_date')[:5]
+
+
+#def index(request):
+#    latest_ques=Question.objects.order_by('-pub_date')[:5]
 #    output=', '.join([p.question_text for p in latest_ques])
-    return render(request, 'polls/index.html', {'latest_question_list':latest_ques})
+#    return render(request, 'polls/index.html', {'latest_question_list':latest_ques})
 
-def detail(request, question_id):
-    question=get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question})
+#def detail(request, question_id):
+#    question=get_object_or_404(Question, pk=question_id)
+#    return render(request, 'polls/detail.html', {'question':question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+#def results(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'polls/results.html', {'question': question})
 
+class IndexView(generic.ListView):
+    template_name='polls/index.html'
+    context_object_name='latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+    model=Question
+    template_name='polls/detail.html'
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    model=Question
+    template_name='polls/results.html'
+    
 def vote(request, question_id):
     p=get_object_or_404(Question, pk=question_id)
     try:
